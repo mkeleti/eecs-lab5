31f23a5648f24783d5e4aeead9aa704a87788788
mkeleti <mmkeleti@gmail.com>
2022-11-04 19:17:38 -0500

    Finalized Password Validator

--- pages/password.tsx
+++ pages/password.tsx
@@ -7,11 +7,28 @@ import Link from "../src/Link";
 import ProTip from "../src/ProTip";
 import Copyright from "../src/Copyright";
 import { Stack, TextField } from "@mui/material";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function About() {
-  const [validated, setValidated] = useState(true);
-
+  const [validated, setValidated] = useState({valid: true, error: ""});
+  const Password1 = useRef<string>();
+  const Password2 = useRef<string>();
+  function Validate() {
+    if (Password1.current?.value != undefined && Password2.current?.value != undefined ) {
+      if (
+        Password1.current.value.length < 8 &&
+        Password2.current.value.length < 8
+      ) {
+        setValidated({valid: false,error: "Passwords must be at least 8 characters"});
+      } else if (Password1.current.value === Password2.current.value) {
+        setValidated({valid: true,error: ""});
+      }
+      else {
+        setValidated({valid: false,error: "Passwords do not match"});
+      }
+    }
+  }
+  let HelperText: string;
   return (
     <Container maxWidth="lg">
       <Box
@@ -26,45 +43,62 @@ export default function About() {
         <Typography variant="h4" component="h1" gutterBottom>
           Password Validator
         </Typography>
-          {validated ? (
-            <Stack spacing={3}> 
-              {" "}
-              <TextField
-                id="outlined-basic"
-                label="Password"
-                type="password"
-                variant="outlined"
-              />
-              <TextField
-                id="outlined-basic2"
-                label="Password"
-                type="password"
-                variant="outlined"
-              />{" "}
-            </ Stack>
-          ) : (
-<Stack spacing={3}>        
+        {validated.valid ? (
+          <Stack spacing={3}>
             {" "}
             <TextField
-            error
+              required
               id="outlined-basic"
               label="Password"
               type="password"
-              helperText="Passwords do not match"
               variant="outlined"
+              inputMode="text"
+              inputRef={Password1}
             />
             <TextField
-            error
+              required
               id="outlined-basic2"
               label="Password"
               type="password"
               variant="outlined"
+              inputMode="text"
+              inputRef={Password2}
             />{" "}
-                  </Stack>
-          )}
+          </Stack>
+        ) : (
+          <Stack spacing={3}>
+            {" "}
+            <TextField
+              error
+              required
+              id="outlined-basic"
+              label="Password"
+              type="password"
+              helperText={validated.error}
+              variant="outlined"
+              inputMode="text"
+              inputRef={Password1}
+            />
+            <TextField
+              error
+              required
+              id="outlined-basic2"
+              label="Password"
+              type="password"
+              variant="outlined"
+              inputMode="text"
+              inputRef={Password2}
+            />{" "}
+          </Stack>
+        )}
 
         <Box pt={3} maxWidth="md">
-          <Button variant="contained" component={Link} noLinkStyle href="/">
+          <Button
+            variant="contained"
+            onClick={() => {
+              Validate();
+            }}
+          >
             Validate Password
           </Button>
         </Box>
